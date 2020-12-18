d6e96b6b291190c8bf0cf21ee36cfc9c9e4f00a1
Kaan Uçar <59536978+kucar17@users.noreply.github.com>
2020-12-18 13:45:46 +0300

    Update Kinematic_Bicycle_Model (2).py

--- Kinematic Bicycle Model/Kinematic_Bicycle_Model (2).py
+++ Kinematic Bicycle Model/Kinematic_Bicycle_Model (2).py
@@ -1,29 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In this notebook, you will implement the kinematic bicycle model. The model accepts velocity and steering rate inputs and steps through the bicycle kinematic equations. Once the model is implemented, you will provide a set of inputs to drive the bicycle in a figure 8 trajectory.
-# 
-# The bicycle kinematics are governed by the following set of equations:
-# 
-# \begin{align*}
-# \dot{x}_c &= v \cos{(\theta + \beta)} \\
-# \dot{y}_c &= v \sin{(\theta + \beta)} \\
-# \dot{\theta} &= \frac{v \cos{\beta} \tan{\delta}}{L} \\
-# \dot{\delta} &= \omega \\
-# \beta &= \tan^{-1}(\frac{l_r \tan{\delta}}{L})
-# \end{align*}
-# 
-# where the inputs are the bicycle speed $v$ and steering angle rate $\omega$. The input can also directly be the steering angle $\delta$ rather than its rate in the simplified case. The Python model will allow us both implementations.
-# 
-# In order to create this model, it's a good idea to make use of Python class objects. This allows us to store the state variables as well as make functions for implementing the bicycle kinematics. 
-# 
-# The bicycle begins with zero initial conditions, has a maximum turning rate of 1.22 rad/s, a wheelbase length of 2m, and a length of 1.2m to its center of mass from the rear axle.
-# 
-# From these conditions, we initialize the Python class as follows:
-
-# In[1]:
-
-
 from notebook_grader import BicycleSolution, grade_bicycle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -50,14 +24,6 @@ class Bicycle():
         self.delta = 0
         self.beta = 0
 
-
-# A sample time is required for numerical integration when propagating the kinematics through time. This is set to 10 milliseconds. We also have a reset function which sets all the state variables back to 0. 
-# 
-# With this sample time, implement the kinematic model using the function $\textit{step}$ defined in the next cell. The function should take speed + angular rate as inputs and update the state variables. Don't forget about the maximum turn rate on the bicycle!
-
-# In[2]:
-
-
 class Bicycle(Bicycle):
     def step(self, v, w):
         # ==================================
@@ -73,33 +39,6 @@ class Bicycle(Bicycle):
         
         pass
         # ==================================
-
-
-# With the model setup, we can now start giving bicycle inputs and producing trajectories. 
-# 
-# Suppose we want the model to travel a circle of radius 10 m in 20 seconds. Using the relationship between the radius of curvature and the steering angle, the desired steering angle can be computed.
-# 
-# \begin{align*}
-#     \tan{\delta} &= \frac{L}{r} \\
-#     \delta &= \tan^{-1}(\frac{L}{r}) \\
-#            &= \tan^{-1}(\frac{2}{10}) \\
-#            &= 0.1974
-# \end{align*}
-# 
-# If the steering angle is directly set to 0.1974 using a simplied bicycled model, then the bicycle will travel in a circle without requiring any additional steering input. 
-# 
-# The desired speed can be computed from the circumference of the circle:
-# 
-# \begin{align*}
-#     v &= \frac{d}{t}\\
-#      &= \frac{2 \pi 10}{20}\\
-#      &= \pi
-# \end{align*}
-# 
-# We can now implement this in a loop to step through the model equations. We will also run our bicycle model solution along with your model to show you the expected trajectory. This will help you verify the correctness of your model.
-
-# In[3]:
-
 
 sample_time = 0.01
 time_end = 20
@@ -137,12 +76,6 @@ print(str(model.delta))
 print(str(solution_model.delta))
 
 
-# The plot above shows the desired circle of 10m radius. The path is slightly offset which is caused by the sideslip effects due to $\beta$. By forcing $\beta = 0$ through uncommenting the last line in the loop, you can see that the offset disappears and the circle becomes centered at (0,10). 
-# 
-# However, in practice the steering angle cannot be directly set and must be changed through angular rate inputs $\omega$. The cell below corrects for this and sets angular rate inputs to generate the same circle trajectory. The speed $v$ is still maintained at $\pi$ m/s.
-
-# In[4]:
-
 
 sample_time = 0.01
 time_end = 20
@@ -177,11 +110,6 @@ plt.plot(x_data, y_data,label='Learner Model')
 plt.plot(x_solution, y_solution,label='Solution Model')
 plt.legend()
 plt.show()
-
-
-# Here are some other example trajectories: a square path, a spiral path, and a wave path. Uncomment each section to view.
-
-# In[5]:
 
 
 sample_time = 0.01
@@ -248,15 +176,6 @@ plt.legend()
 plt.show()
 
 
-# We would now like the bicycle to travel a figure eight trajectory. Both circles in the figure eight have a radius of 8m and the path should complete in 30 seconds. The path begins at the bottom of the left circle and is shown in the figure below:
-# 
-# ![title](figure8.png)
-# 
-# Determine the speed and steering rate inputs required to produce such trajectory and implement in the cell below. Make sure to also save your inputs into the arrays v_data and w_data, these will be used to grade your solution. The cell below also plots the trajectory generated by your own model.
-
-# In[48]:
-
-
 sample_time = 0.01
 time_end = 30
 model.reset()
@@ -309,28 +228,12 @@ plt.plot(x_data, y_data)
 plt.show()
 
 
-# We will now run your speed and angular rate inputs through our bicycle model solution. This is to ensure that your trajectory is correct along with your model. The cell below will display the path generated by our model along with some waypoints on a desired figure 8. Surrounding these waypoints are error tolerance circles with radius 1.5m, your solution will pass the grader if the trajectory generated stays within 80% of these circles.
-
-# In[47]:
-
-
 grade_bicycle(t_data,v_data,w_data)
 
-
-# The cell below will save the time and vehicle inputs as text file named $\textit{figure8.txt}$. To locate the file, change the end of your web directory to $\textit{/notebooks/Course_1_Module_4/figure8.txt}$
-# 
-# Once you are there, you can download the file and submit to the Coursera grader to complete this assessment.
-
-# In[50]:
 
 
 data = np.vstack([t_data, v_data, w_data]).T
 np.savetxt('figure8.txt', data, delimiter=', ')
-
-
-# Congratulations! You have now completed the assessment! Feel free to test the bicycle model with different inputs in the cell below, and see what trajectories they form. For example, try moving in an equilateral triangle. You'll find that it's rather difficult to generate desired trajectories by pre-setting the inputs. The next module on vehicle control will show you an easier and more accurate method. See you there!
-
-# In[51]:
 
 
 sample_time = 0.01
@@ -353,10 +256,3 @@ for i in range(t_data.shape[0]):
 plt.axis('equal')
 plt.plot(x_data, y_data)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
